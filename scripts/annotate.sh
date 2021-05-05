5987fa7ad17082da6ef81da1dad9dce1b34b7218
Gyorgy Orosz <gyorgy@orosz.link>
2021-05-05 12:03:02 +0000

    Adjusted annotate.sh

--- scripts/annotate.sh
+++ scripts/annotate.sh
@@ -4,16 +4,16 @@ VERSION="4.0.6"
 
 input_file=$1
 output_file=$2``
-tmp_file=/tmp/emtsv/$(basename "$input_file")
+tmp_file=./tmp/emtsv/$(basename "$input_file")
 
 echo "Processing \"$input_file\""
-mkdir -p /tmp/emtsv
+mkdir -p ./tmp/emtsv
 
 # Store emtsv results in a temp file
 awk 'BEGIN {print "form"} {print}' "${input_file}" \
   | grep -v "^#" \
   | cut -f 1 \
-  | docker run --rm -i mtaril/emtsv:${VERSION} emMorph,emTag,emmorph2ud 2>/dev/null \
+  | docker run --rm -i mtaril/emtsv:${VERSION} emMorph,emTag,emmorph2ud 2>>error.log \
   | awk -F $'\t' 'BEGIN {OFS = FS} FNR>1 {print $3,$5,$4,$6}' > "$tmp_file"
 
 # Merge the original file w/ the emtsv result
